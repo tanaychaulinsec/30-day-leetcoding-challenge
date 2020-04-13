decdd3320518d1c33ac4f282e2395c164e2c1ce1
Tanay <tanaychauli@gmail.com>
2020-04-13 19:14:42 +0530

    add++

--- contiguousArray.py
+++ contiguousArray.py
@@ -1,13 +1,12 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         nums=[x if x else -1 for x in nums]
-        prefixSum={0:-1}
+        dict={0:-1}
         presum=res=0
         for i,x in enumerate(nums):
             presum+=x
-            if presum in prefixSum:
-                res=max(res,i-prefixSum[presum])
+            if presum in dict:
+                res=max(res,i-dict[presum])
             else:
-                prefixSum[presum]=i
+                dict[presum]=i
         return res
-        
