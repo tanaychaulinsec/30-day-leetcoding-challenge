4e9cab9a4c4db342efec98f6a3cd2bf725fc176e
tanaychaulinsec <tanaychauli@gmail.com>
2020-04-30 16:44:06 +0530

    add+

--- CheckValidSequenceInTreeRootToLeaf.py
+++ CheckValidSequenceInTreeRootToLeaf.py
@@ -1,13 +1,10 @@
 class Solution:
     def isValidSequence(self, root: TreeNode, arr: List[int]) -> bool:
         def dfs(root,index):
-            size=len(arr)
-            if not root or index == size or arr[index] != root.val:
+            n= len(arr)
+            if not root or index==n or arr[index]!=root.val:
                 return False
-            if index == size - 1 and not (root.left or root.right):
-                return True
-            return ((index < size-1) and (root.val == arr[index]) and 
-            (dfs(root.left,index + 1) or
-             dfs(root.right,index + 1)))
-        
+            if index== n-1 and not(root.left or root.right):
+                return  True
+            return (dfs(root.left,index+1) or dfs(root.right,index+1))
         return dfs(root,0)
